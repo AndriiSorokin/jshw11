c9e02f45c86ca14b822e7b38878b4e9822cc015c
AndriiSorokin <68332371+AndriiSorokin@users.noreply.github.com>
2021-01-21 14:13:20 +0200

    сделал слушателей и переключатель

--- switch-color/color.js
+++ switch-color/color.js
@@ -1,7 +1,7 @@
-const startBtn = document.querySelector('[data-action="start"]')
-const stopBtn = document.querySelector('[data-action="stop"]')
-
-
+const startBtn = document.querySelector('[data-action="start"]');
+const stopBtn = document.querySelector('[data-action="stop"]');
+const body = document.querySelector('body');
+let bodyColor;
 const colors = [
   '#FFFFFF',
   '#2196F3',
@@ -14,3 +14,20 @@ const colors = [
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const startColor = () => {
+  bodyColor = setInterval(() => {
+    const color = randomIntegerFromInterval(0, colors.length - 1)
+    body.style.backgroundColor = colors[color]
+  }, 1000)
+  startBtn.disabled = true
+}
+
+const stopColor = () => {
+  clearInterval(bodyColor)
+  startBtn.disabled = false
+}
+
+startBtn.addEventListener('click', startColor);
+stopBtn.addEventListener('click', stopColor)
+
