659f09bf48c3d39dbf185931b49d246fa5dfe2a8
AndriiSorokin <68332371+AndriiSorokin@users.noreply.github.com>
2021-01-21 13:50:03 +0200

    сделал разметку

---
+++ switch-color/color.js
@@ -0,0 +1,16 @@
+const startBtn = document.querySelector('[data-action="start"]')
+const stopBtn = document.querySelector('[data-action="stop"]')
+
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
